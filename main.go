b850559d1c27523fd936a2094c7d8a185c19b56d
spushan <brent_002@yahoo.com>
2020-06-18 20:32:54 -0700

    ssemi

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"sync"
 	"time"
@@ -88,27 +89,67 @@ func populate(r int, c int, p float64) (m [][]float64) {
 	return
 }
 
+func maker(row, col, row2, col2 int, p float64) (matrix, matrix, matrix, error) {
+
+	mat := matrix{1, 1, populate(1, 1, 1)}
+
+	if row == 0 || col == 0 || row2 == 0 || col2 == 0 {
+		return mat, mat, mat, errors.New("0 for size not allowed")
+	}
+	if col != row2 {
+		return mat, mat, mat, errors.New("Column of Matrix 1 and Row of Matrix 2 must be Equal")
+	}
+	mat1 := matrix{row, col, populate(row, col, p)}
+	mat2 := matrix{row2, col2, populate(row2, col2, p)}
+	answ := matrix{row, col2, populate(row, col2, 0)}
+
+	return mat1, mat2, answ, nil
+}
+
 func main() {
-	mat1 := matrix{100, 100, populate(100, 100, 1)}
-	mat2 := matrix{100, 100, populate(100, 100, 1)}
-	answ := matrix{100, 100, populate(100, 100, 0)}
 
-	//mat1.print()
-	//mat2.print()
+	var row, col, row2, col2 int
+	var p float64
 
-	fmt.Println("NORMAL MULTIPLY")
-	start := time.Now()
-	answ.mat = mat1.multiply(mat2)
-	t := time.Now()
-	elapsed := t.Sub(start)
-	fmt.Printf("Time Elapsed Normal: %v\n", elapsed)
-	//answ.print()
+	for true {
 
-	fmt.Println("PARALLEL MULTIPLY")
-	start = time.Now()
-	answ.mat = mat1.pmultiply(mat2)
-	t = time.Now()
-	elapsed = t.Sub(start)
-	fmt.Printf("Time Elapsed Normal: %v\n", elapsed)
-	//answ.print()
+		fmt.Print("Enter First Matrix Row: ")
+		fmt.Scanln(&row)
+		fmt.Print("Enter First Matrix Column: ")
+		fmt.Scanln(&col)
+		fmt.Print("Enter Second Matrix Row: ")
+		fmt.Scanln(&row2)
+		fmt.Print("Enter Second Matrix Column: ")
+		fmt.Scanln(&col2)
+		fmt.Print("Enter Number to populate with: ")
+		fmt.Scanln(&p)
+
+		mat1, mat2, answ, err := maker(row, col, row2, col2, p)
+		if err != nil {
+
+			fmt.Println("NORMAL MULTIPLY")
+			start := time.Now()
+			answ.mat = mat1.multiply(mat2)
+			t := time.Now()
+			elapsed := t.Sub(start)
+			fmt.Printf("Time Elapsed Normal: %v\n", elapsed)
+
+			if row < 10 && col < 10 {
+				answ.print()
+			}
+
+			fmt.Println("PARALLEL MULTIPLY")
+			start = time.Now()
+			answ.mat = mat1.pmultiply(mat2)
+			t = time.Now()
+			elapsed = t.Sub(start)
+			fmt.Printf("Time Elapsed Normal: %v\n", elapsed)
+
+			if row < 10 && col < 10 {
+				answ.print()
+			}
+		}
+
+	}
+
 }
